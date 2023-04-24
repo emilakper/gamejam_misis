26b6ab88fe7e3262cb55872bb23984c993ca9077
emilakper <emil-akper@yandex.ru>
2023-04-24 15:22:23 +0300

    Create Project

---
+++ Scripts/test.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class HelloWorld : MonoBehaviour
+{
+    //Variables
+    //Functions
+    //Classes    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

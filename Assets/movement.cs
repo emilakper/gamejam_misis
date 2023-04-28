f67c1ee92a6534e972744abb997f567390fb23ff
dueip <68518940+dueip@users.noreply.github.com>
2023-04-28 19:25:44 +0300

    Refactored moving script, by moving legs' movement into a function

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -10,12 +11,12 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
 
-    public KeyCode left_leg = KeyCode.F;
-    public KeyCode right_leg = KeyCode.G;
-    public KeyCode left_arm = KeyCode.J;
-    public KeyCode right_arm = KeyCode.K;
-    public KeyCode breath = KeyCode.O;
-    public KeyCode blink = KeyCode.E;
+    public KeyCode _left_leg = KeyCode.F;
+    public KeyCode _right_leg = KeyCode.G;
+    public KeyCode _left_arm = KeyCode.J;
+    public KeyCode _right_arm = KeyCode.K;
+    public KeyCode _breath = KeyCode.O;
+    public KeyCode _blink = KeyCode.E;
 
 
     private class Arm
@@ -24,12 +25,12 @@ public class movement : MonoBehaviour
     }
 
 
-    enum Limb { None = 0x0, Right = 0x1, Left = 0x2 }
+    enum Limb  : uint { Right = 0x0, Left = 4294967295 }
 
     class Actions
     {
-        public Limb leg = Limb.None;
-        public Limb arm = Limb.None;
+        public Limb leg = Limb.Right;
+        public Limb arm = Limb.Right;
         public double time_since_blink = 0;
         public double time_since_breath = 0;
     }
@@ -42,20 +43,61 @@ public class movement : MonoBehaviour
         
     }
 
+
+    // Returns time since last blink
+    private double blink()
+    {
+        double temp = action.time_since_blink;
+        action.time_since_blink = 0;
+        return temp;
+    }
+
+    private double breath() 
+    {
+        double temp = action.time_since_breath;
+        action.time_since_blink = 0;
+        return temp;
+    }
+
+    private void move_leg(Vector3 direction, Limb prev_limb, Limb curr_limb)
+    {
+        if (prev_limb == curr_limb)
+        {
+            return;
+        }
+
+        if (prev_limb == Limb.Right)
+        {
+            transform.position += direction;
+            action.leg = Limb.Left;
+        }
+        else
+        {
+            transform.position += direction;
+            action.leg = Limb.Right;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(right_leg) && (action.leg == Limb.Right || action.leg == Limb.None))
+        if (Input.GetKeyDown(_right_leg))
         {
-            transform.position += Vector3.right;
-            action.leg = Limb.Left;
+           move_leg(Vector3.right, action.leg, Limb.Right);
         }
-        if (Input.GetKeyDown(left_leg) && (action.leg == Limb.Left || action.leg == Limb.None))
+        else if (Input.GetKeyDown(_left_leg))
         {
-            transform.position += Vector3.right;
-            action.leg = Limb.Right;
+            move_leg(Vector3.right, action.leg, Limb.Left);
         }
 
+        if (Input.GetKeyDown(_breath))
+        {
+            breath();
+        }
+
+        if (Input.GetKeyDown(_blink))
+        {
+            blink();
+        }
 
         action.time_since_breath += Time.deltaTime;
         action.time_since_blink += Time.deltaTime;

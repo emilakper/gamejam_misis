7606e32f7a23f2bfdd47df932ee2b6cc2f0760b7
dueip <68518940+dueip@users.noreply.github.com>
2023-04-29 12:56:14 +0300

    Changed folders

---
+++ Assets/Scripts/CoffeeMachine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour, Actionable
+{
+    public KeyCode _action_button = KeyCode.A;
+    void Start()
+    {
+    }
+    void Update()
+    {
+
+    }
+
+    public void actOn(movement player, KeyCode action_key)
+    {
+        if (Input.GetKeyDown(action_key))
+        {
+            print("I am coffee Machineeee\n");
+        }
+    }
+    public void preActOn(movement player)
+    {
+        player._action_button = _action_button;
+    }
+
+
+    public void postActOn(movement player)
+    {
+
+    }
+
+}

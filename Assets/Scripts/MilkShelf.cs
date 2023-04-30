cd3bfc00c259d948545074938d3589b4d30ed60e
dueip <68518940+dueip@users.noreply.github.com>
2023-04-30 15:12:55 +0300

    z

--- Assets/Scripts/MilkShelf.cs
+++ Assets/Scripts/MilkShelf.cs
@@ -7,7 +7,7 @@ public class MilkShelf : MonoBehaviour, Actionable
     public void preActOn(movement mov) { }
     public void actOn(movement mov, KeyCode key) 
     { 
-        if (Input.GetKeyDown(key))
+        if (Input.GetKeyDown(key) && (!player.left_arm.is_empty() || !player.right_arm.is_empty()))
         {
             mov.add_milk();
         }

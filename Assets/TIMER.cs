496717290b857f2d763f69efc9ea710fc0af6909
dueip <68518940+dueip@users.noreply.github.com>
2023-04-30 14:42:43 +0300

    Timer works

--- Assets/TIMER.cs
+++ Assets/TIMER.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TIMER : MonoBehaviour
 {
+    public bool is_active = false; 
     public double time_to_wait = 0;
     Vector3 base_vec;
 
@@ -21,11 +22,19 @@ public class TIMER : MonoBehaviour
     {
         time_sprite = transform.GetChild(1).gameObject;
         base_vec = time_sprite.transform.localScale;
+        gameObject.SetActive(false);
+    }
+
+    public void res()
+    {
+        time_sprite = transform.GetChild(1).gameObject;
+        time_sprite.transform.localScale = base_vec;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!is_active) return;
         Vector3 a = time_sprite.transform.localScale;
         if (a.x > 0) 
             a.x -= base_vec.x * (float)(Time.deltaTime/ time_to_wait);
